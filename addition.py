11d83821b633b1b5cf9eb3df84587776c7cd29af
Oscar Stanley <40785@longroad.ac.uk>
2014-10-22 15:06:41 +0100

    done

---
+++ addition.py
@@ -0,0 +1,11 @@
+#oscar stanley
+#22/10/2014
+#addition
+total = 0
+for number in range(1,9):
+    num = int(input("Enter number {0}: ".format(number)))
+    total = total + num
+   
+print(total)
+
+    
