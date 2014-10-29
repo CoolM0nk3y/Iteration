c3df220ad3ccf1d5ac681521335b887198c09b6f
oscar <oscar@CoolM0nk3y>
2014-10-29 19:55:53 +0000

    done

---
+++ average of the numbers.py
@@ -0,0 +1,15 @@
+total = 0
+amount = int(input("Enter the amount of numbers you want to average:"))
+
+for number in range(1,amount + 1):
+    enterd = int(input("please enter a number: "))
+    total = total + enterd
+
+
+subtotal = total / amount 
+print("your ancwer is {0}".format(subtotal))
+    
+                
+    
+   
+    
