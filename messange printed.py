6df2f5a1749b5871b69d505c0cd900f02b58b5a6
oscar <oscar@CoolM0nk3y>
2014-10-30 15:56:04 +0000

    done

---
+++ messange printed.py
@@ -0,0 +1,10 @@
+total = 0
+amount = int(input("Enter the amount of times you want the message displyed:"))
+message = input("please enter the message you want to be displayed {0} times: ".format(amount)) 
+
+for messagedis in range(1,amount + 1):
+    
+    print("{0}".format(message))
+    
+
+
