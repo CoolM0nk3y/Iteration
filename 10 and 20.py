48225ed82278148bc5f89963e33ecf1dc327938d
oscar <oscar@CoolM0nk3y>
2014-11-01 14:25:20 +0000

    done

---
+++ 10 and 20.py
@@ -0,0 +1,6 @@
+number = int(input("Pease enter a number in the range 10-20: "))
+
+while not 10 <= number <= 20 :
+    number = int(input("Please enter a number in the range 10-20:"))
+    if 10 <= number <= 20:
+        print("Your number is in the rand of 10-20")
